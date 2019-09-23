f7ac910ebd9894bb583e907f772991ad99b64e24
Jeremias Volker <jeremias.volker@googlemail.com>
2019-09-23 17:42:56 +0200

    Fixes install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 # Be VERY Careful. This script may be executed with admin privileges.
 
-echo "Installing processing..."
+echo "Installing Processing extension ..."
 
 if ! [ -z "$TRAVIS" ]; then
     echo "TRAVIS env, don't install"
@@ -12,7 +12,7 @@ fi
 os=$(uname)
 arq=$(uname -m)
 
-# does glslViewer already exist?
+# does processing-java already exist?
 if hash processing-java 2>/dev/null; then
     echo "processing-java already installed."
     exit 0
@@ -24,17 +24,11 @@ if [ $os == "Linux" ]; then
     # on RaspberryPi
     if [ $arq == "armv7l" ]; then
         curl https://processing.org/download/install-arm.sh | sudo sh
-        sudo cp ./bin/opf-processing.sh /usr/local/bin/
-        sudo chmod +x /usr/local/bin/opf-processing.sh
     else
-      echo "Normal install processing https://processing.org/download/"
+      echo "Install Processing manually from: https://processing.org/download/"
     fi
-
-
 elif [ $os == "Darwin" ]; then
 
     # ON MacOX
-    echo "osx"
-    echo "Normal install processing https://processing.org/download/"
-
+    echo "Install Processing manually from: https://processing.org/download/"
 fi
